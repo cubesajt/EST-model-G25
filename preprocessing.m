9f51d1fff09d85e320f3ac911354c0e93ef1492e
Adam Gregus <a.gregus@student.tue.nl>
2024-05-15 14:45:37 +0200

    setup

---
+++ preprocessing.m
@@ -0,0 +1,43 @@
+% Pre-processing script for the EST Simulink model. This script is invoked
+% before the Simulink model starts running (initFcn callback function).
+
+%% Load the supply and demand data
+
+timeUnit   = 's';
+
+supplyFile = "Team25_supply.csv";
+supplyUnit = "MW";
+
+% load the supply data
+Supply = loadSupplyData(supplyFile, timeUnit, supplyUnit);
+
+demandFile = "Team25_demand.csv";
+demandUnit = "MW";
+
+% load the demand data
+Demand = loadDemandData(demandFile, timeUnit, demandUnit);
+
+%% Simulation settings
+
+deltat = 5*unit("min");
+stopt  = min([Supply.Timeinfo.End, Demand.Timeinfo.End]);
+
+%% System parameters
+
+% transport from supply
+aSupplyTransport = 0.0; % Dissipation coefficient
+
+% injection system
+aInjection = 0.2; % Dissipation coefficient
+
+% storage system
+EStorageMax     = 13.1.*unit("GWh"); % Maximum energy
+EStorageMin     = 0.0*unit("kWh"); % Minimum energy
+EStorageInitial = 0.0*unit("kWh"); % Initial energy
+bStorage        = 1e-6/unit("s");  % Storage dissipation coefficient
+
+% extraction system
+aExtraction = 0.1; % Dissipation coefficient
+
+% transport to demand
+aDemandTransport = 0.01; % Dissipation coefficient
