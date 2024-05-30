d0b2fd8bef3b5114712b561338d065a6e7a53cba
archiefly <93826607+archiefly@users.noreply.github.com>
2024-05-30 15:05:07 +0200

    Update and rename pipe_pressure_drop_per_unit_length.m to pipe_pressure_drop.m
    parameters changed for our validation experiment

--- pipe_pressure_drop.m
+++ pipe_pressure_drop.m
@@ -1,9 +1,9 @@
 DynamicViscosity = 18.13 * 10 ^ (-6); % dynamic viscocity of fluid (Pa s) at respective flow temperature (assumed 293K)
-Flowrate = ?; % flow rate of fluid (m^3 / s)
-Length = 1; % length of the pipe (m)
+Flowrate = 120 / 60000; % flow rate of fluid (m^3 / s)
+Length = 0.3; % length of the pipe (m)
 Density = 1.225; % density of the fluid (kg/m^3)
-Diameter = ? 0.002; % diameter of the pipe (m)
-Roughness = 0.0015 * 10 ^ (-3); % pipe roughness coefficient (m)
+Diameter = 0.004; % diameter of the pipe (m)
+Roughness = 0.0015 * 10 ^ (-6); % pipe roughness coefficient (m)
 
 disp(['The pressure drop for your specified parameters is: ', num2str(PressureDrop(Length, Diameter, DynamicViscosity, Flowrate, Density, Roughness)), ' Pa']);
 
@@ -17,7 +17,7 @@ function velocity = Velocity(FlowRate, Diameter)
     % Returns:
     % velocity: The mean velocity of the fluid flowing through the pipe (in meters per second).
 
-    velocity = 4 * FlowRate / (pi * Diameter^2);
+    velocity = 4 * FlowRate / (pi * Diameter ^ 2);
 end
 
 function ReynoldsNumber = ReynoldsNumber(Density, Length, Viscosity, Flowrate, Diameter)
@@ -55,8 +55,6 @@ function DarcyFrictionFactor = SolveColebrook(Roughness, Diameter, Density, Leng
     DarcyFrictionFactor = fzero(Colebrook, [0.005, 0.1]);
 end
 
-text = 'Pipe Pressure Drop per Unit Length is :';
-disp(text);
 function PressureDrop = PressureDrop(Length, Diameter, Viscosity, Flowrate, Density, Roughness)
     % Uses a rearranged Darcy-Weisbach equation to solve the pipe diameter for a laminar flow of given diameters. 
     % The Darcy friction fraction has already been substituted for a circular pipe diameter.
